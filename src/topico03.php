e7140df2f4c78e6a1863ef6804b9ee1284dc469f
Gustavo Antonio <93854241+Non-Gustavo19@users.noreply.github.com>
2026-09-09 20:36:55 -0300

    Atualizar o topico03.php

--- src/topico03.php
+++ src/topico03.php
@@ -4,9 +4,14 @@ echo "cep = $cep";
 $valor=5124.784;
 echo "<br>Valor = $valor";
 echo "<br>R$ ".number_format($valor,2,",",".");
+echo "<br>R$ ".number_format($valor,0,",",".");
 
 /*
 R$ 5.124,74
+R$ 5.125
 /*
 
+$nome="bete";
+echo "<br>Nome = $nome";
+echo '<br>Nome = $nome';
 ?>
