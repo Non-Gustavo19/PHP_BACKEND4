664c44d665c1ebccffec267d0c91c1db2f36a67f
Gustavo Antonio <93854241+Non-Gustavo19@users.noreply.github.com>
2026-09-09 20:24:46 -0300

    Atualizar o topico03.php

--- src/topico03.php
+++ src/topico03.php
@@ -3,7 +3,7 @@ $cep="01012123";
 echo "cep = $cep";
 $valor=5124.784;
 echo "<br>Valor = $valor";
-echo "<br>R$ ".number_format($valor,2);
+echo "<br>R$ ".number_format($valor,2,",",".");
 /*
 R$ 5.124,74
 /*
