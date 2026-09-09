d08b708c8fc76dd9d98821c01a958fa3a0b5453f
Gustavo Antonio <93854241+Non-Gustavo19@users.noreply.github.com>
2026-09-09 20:55:43 -0300

    Atualizar o topico03.php

--- src/topico03.php
+++ src/topico03.php
@@ -5,15 +5,17 @@ $valor=5124.784;
 echo "<br>Valor = $valor";
 echo "<br>R$ ".number_format($valor,2,",",".");
 echo "<br>R$ ".number_format($valor,0,",",".");
-
 /*
 R$ 5.124,74
 R$ 5.125
-/*
-
+*/
 $nome="bete";
 echo "<br>Nome = $nome";
 echo '<br>Nome = $nome';
 $cor="green";
 echo "<p style='color:$cor'>$nome</p>";
 ?>
+/*
+valor = 5124
+valor = 0.784
+*/
